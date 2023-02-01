ee3b0ada7e203ea4a06da796189e02f4d2f13967
Jamison Fox <fox.jamison@outlook.com>
2023-02-01 14:49:52 -0700

    created index.js file and connected to server.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// require your server and launch it
+const server = require('./api/server.js');
+
+const hostname = '127.0.0.1';
+const port = 8000;
+
+server.listen(port, hostname, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+    console.log(`Server listening on http://${hostname}:${port}/`);
+})
