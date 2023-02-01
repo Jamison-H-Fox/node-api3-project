f8e21e775bdb1f45fc937c2eb16cb3f6344c1f74
Jamison Fox <fox.jamison@outlook.com>
2023-02-01 16:05:07 -0700

    validatePost and ValidateUser middleware built out

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -28,14 +28,28 @@ async function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  console.log('validateUser middleware')
-  next()
+  const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing required name field',
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
   // DO YOUR MAGIC
 }
 
 function validatePost(req, res, next) {
-  console.log('validatePost middleware')
-  next()
+  const { text } = req.body;
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: 'missing required text field',
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
   // DO YOUR MAGIC
 }
 
